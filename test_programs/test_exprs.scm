216580198b33b6e6df1da92e835c39190d5a60f3
drkwitht <derektan3510@gmail.com>
2025-04-08 14:02:56 -0700

    Expanded simple Scheme sample.

--- test_programs/test_exprs.scm
+++ test_programs/test_exprs.scm
@@ -1,2 +1,8 @@
-(define my-number 42)
-(display my-number)
+; test_exprs.scm
+(define x 42)
+(define y 24)
+(define (my-average a b)
+    (+ (/ a 2)
+    (/ b 2)))
+(define answer (my-average x y))
+(display answer)
